3a3d802973afd31a8f30ff2827ad1143924f6a38
Hugh Gilmour <shugster13@gmail.com>
2023-04-18 08:29:52 +0100

    Add increment_quality and refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -20,14 +20,10 @@ class GildedRose
             item.quality = item.quality + 1
             if item.name == "Backstage passes to a TAFKAL80ETC concert"
               if item.sell_in < 11
-                if item.quality < 50
-                  item.quality = item.quality + 1
-                end
+                increment_quality(item)
               end
               if item.sell_in < 6
-                if item.quality < 50
-                  item.quality = item.quality + 1
-                end
+                increment_quality(item)
               end
             end
           end
@@ -43,9 +39,7 @@ class GildedRose
               item.quality = item.quality - item.quality
             end
           else
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
+            increment_quality(item)
           end
         end
       end
@@ -60,6 +54,13 @@ class GildedRose
   def decrement_quality(item, amount)
      item.quality = item.quality - amount < 0 ? 0 : item.quality - amount
      item
+  end
+
+  def increment_quality(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+    end
+    item
   end
 end
 
